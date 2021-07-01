d7806244bc5ecce5a9b502d7e3bab9c68af30c26
minwin97 <86585450+minwin97@users.noreply.github.com>
2021-07-01 11:25:29 +0900

    Create 01.kotlin.kt

---
+++ 01.kotlin.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.안드로이드_UI
+
+//01. 변수
+
+// 변수 선언하는 방법
+// var/val 변수명(상자) = 값(넣고싶은 것)
+// var(variable) : 내 마음대로 원하는 것을 넣을 수 있는 상자
+// val(value) : 한 번 넣으면 바꿀 수 없는 상자
+
+var num = 10
+fun main(args:Array<String>){
+
+}
