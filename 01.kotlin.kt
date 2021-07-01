0cbcba97490fe717b9cc117a708a6d006948f5de
minwin97 <86585450+minwin97@users.noreply.github.com>
2021-07-01 11:51:55 +0900

    Update 01.kotlin.kt

--- 01.kotlin.kt
+++ 01.kotlin.kt
@@ -8,6 +8,10 @@ package com.example.myapplication.안드로이드_UI
 // val(value) : 한 번 넣으면 바꿀 수 없는 상자
 
 var num = 10
+var hello = "hello"
+var point = 3.4
+
+val fix = 20
 fun main(args:Array<String>){
 
 }
